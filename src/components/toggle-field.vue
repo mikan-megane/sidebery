4558f1b0015907c757c97454cb7306f577635818
mbnuqw <maxbadryzlov@gmail.com>
2024-12-06 20:45:10 +0500

    feat: toggle field: allow links in the note message

--- src/components/toggle-field.vue
+++ src/components/toggle-field.vue
@@ -3,7 +3,11 @@
   .body
     .label(:style="{ color: props.color }") {{translate(props.label)}}
     ToggleInput.input(:value="props.value")
-  .note(v-if="props.note") {{props.note}}
+  .note(v-if="props.note" @click.stop="") {{props.note}}
+  .note(v-if="props.noteWithLinks" @click.stop="")
+    template(v-for="v, i in getNoteWithLinksParts(props.noteWithLinks)")
+      template(v-if="!(i%2)") {{v}}
+      a(v-else :href="getHref(v)") {{getStr(v)}}
   slot
 </template>
 
@@ -18,6 +22,7 @@ interface ToggleFieldProps {
   field?: boolean
   color?: string
   note?: string
+  noteWithLinks?: string
 }
 
 const emit = defineEmits(['toggle', 'update:value'])
@@ -27,5 +32,19 @@ function toggle(): void {
   if (props.inactive) return
   emit('update:value', !props.value)
   emit('toggle', !props.value)
+}
+
+function getNoteWithLinksParts(note: string): string[] {
+  return note.split(/<a |<\/a>/)
+}
+
+const hrefRe = /href="(.*)"/
+function getHref(str: string): string {
+  return hrefRe.exec(str)?.[1] ?? ''
+}
+
+const strRe = />(.*)/
+function getStr(str: string): string {
+  return strRe.exec(str)?.[1] ?? ''
 }
 </script>
